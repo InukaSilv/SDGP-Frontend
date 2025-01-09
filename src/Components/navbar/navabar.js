fc71d26c53e3a265057efc55a13d14be4d1864c8
Ishara Dharmapriya <isharadharmapriya145@gmail.com>
2025-01-09 18:51:13 +0530

    navbar update
    nav bar updated without adjustments for different screen sizes

--- src/Components/navbar/navabar.js
+++ src/Components/navbar/navabar.js
@@ -1,31 +1,38 @@
+import { Component } from "react";
 import "./navbar.css";
+import PersonIcon from '@mui/icons-material/Person';
 
-function Navbar() {
-  return <>
-  <nav>
-    <div>
-    <div className="navbar-container">
-<div className="navbar-logo">
-      <h1>RiVVE</h1>
-    </div>
-    <div className="navbar-menu">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/hostel">Hostel</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </div>
-    </div>
-    </div>
-    <div className="navbar-button">
-        <h3>Already a member?</h3>
-        <button>Login</button>  
-        <button>Signup</button>
-    </div>
-    
-    </nav>
-  </>;
+class Navbar extends Component {
+state={clicked:false};
+
+handleClick = () =>{
+  this.setState({clicked:!this.state.clicked});
+}
+render(){
+return (
+    <>
+      <nav className="nav-bar">
+        <section className="nav-left">
+          <h1>RiVVE</h1>
+          <ul id="navbar" className="">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Hostels</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </section>
+        <section className="nav-right">
+          <p>Already a member?</p>
+          <button>LOGIN</button>
+          <button>SIGNUP</button>
+
+          <PersonIcon fontSize="large" className="person-icon"/>
+        </section>
+      </nav>
+    </>
+  );
+}
+  
 }
 
 export default Navbar;
