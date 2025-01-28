a2359603e77009d7e8e311e83e2f7d3f09957454
Ishara Dharmapriya <isharadharmapriya145@gmail.com>
2025-01-28 10:09:54 +0530

    last
    last

---
+++ src/Pages/Browse/Browse.tsx
@@ -0,0 +1,14 @@
+import { Component, ReactNode } from "react";
+import Navbar from "../../Components/navbar/navabar";
+
+class Browse extends Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+      </>
+    );
+  }
+}
+
+export default Browse;
