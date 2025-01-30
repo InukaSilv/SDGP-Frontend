ac5326735193774ac2eb75fc664a1323f7f51bc6
Ishara Dharmapriya <isharadharmapriya145@gmail.com>
2025-01-30 20:49:34 +0530

    sample

---
+++ RiVVe/src/pages/Signup1.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar/navbar";
+
+function Signup1() {
+  return (
+    <>
+      <Navbar />
+      <section></section>
+    </>
+  );
+}
+export default Signup1;
