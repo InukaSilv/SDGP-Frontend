9430539d4065ce0e64466605df6756d8a21a12b0
Lasal0425 <lasalrathnayake@gmail.com>
2025-02-05 19:39:32 +0530

    started on about us page

---
+++ RiVVe/src/components/teamCards/TeamCard.tsx
@@ -0,0 +1,11 @@
+const TeamCard = () => {
+  return (
+    <div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Lasal Rathnayake</h1>
+        <p>Front end developer</p>
+      
+    </div>
+  )
+}
+
+export default TeamCard
--- RiVVe/src/pages/About.tsx
+++ RiVVe/src/pages/About.tsx
@@ -1,9 +1,18 @@
-import Navbar from "../components/navbar/navbar";
+import Navbar from "../components/navbar/Navbar";
+import TeamCard from "../components/teamCards/teamCard";
+
 
 function About() {
   return (
     <>
-      <Navbar />
+      <Navbar/>
+
+      <div className="mt-30 flex flex-col items-center gap-10">
+        <h1 className="text-3xl font-bold">About us</h1>
+        <p className="text-slate-700">This section will tell the details of our teams and follow us on social media for more details</p>
+         {/*Here using the component teamCard*/}
+         <TeamCard/>
+      </div>
     </>
   );
 }
