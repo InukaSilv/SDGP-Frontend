e37fb5da73a1f91311030cc8d0b4b95e60168e2f
Lasal0425 <lasalrathnayake@gmail.com>
2025-02-07 20:18:49 +0530

    Added all team members with their assigned role and added RiVVE's primary goal

--- RiVVe/src/components/teamCards/TeamCard.tsx
+++ RiVVe/src/components/teamCards/TeamCard.tsx
@@ -1,10 +1,32 @@
 const TeamCard = () => {
   return (
-    <div className="w-40 h-60 border-2 flex flex-col items-center">
-        <h1>Lasal Rathnayake</h1>
+    <><div className="w-40 h-60 border-2 flex flex-col items-center">
+      <h1>Lasal Rathnayake</h1>
+      <p>Front end developer</p>
+    </div><div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Ishara Dharmapriya</h1>
         <p>Front end developer</p>
+    </div><div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Inuka </h1>
+        <p>Team Leader</p>
+        </div>
+        <div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Dulen Eragoda </h1>
+        <p>Back End Developer</p>
+        </div>
+        <div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Dulen Perera </h1>
+        <p>Back End Developer</p>
+        </div>
+        <div className="w-40 h-60 border-2 flex flex-col items-center">
+        <h1>Hasindu Perera</h1>
+        <p>AI Developer</p>
+        
+
+
+
+      </div></>
       
-    </div>
   )
 }
 
--- RiVVe/src/pages/About.tsx
+++ RiVVe/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/navbar/Navbar";
-import TeamCard from "../components/teamCards/teamCard";
+import TeamCard from "../components/teamCards/TeamCard";
 
 
 function About() {
@@ -11,8 +11,20 @@ function About() {
         <h1 className="text-3xl font-bold">About us</h1>
         <p className="text-slate-700">This section will tell the details of our teams and follow us on social media for more details</p>
          {/*Here using the component teamCard*/}
-         <TeamCard/>
+
+         {/* Horizontal layout for team cards */}
+        <div className="flex flex-row flex-wrap justify-center gap-6">
+          <TeamCard />
+        </div>
       </div>
+
+
+      <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+        <h1>About <span>Our Brand</span></h1>
+        <p>RiVVE’s primary goal is to become the preferred resource for students seeking housing by aiming to solve accessibility, transparency, and convenience barriers by delivering a single platform designed specifically for students.</p>
+        
+      </div>
+
     </>
   );
 }
