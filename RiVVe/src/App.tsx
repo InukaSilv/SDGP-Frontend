e1c76b9f3c6cf629285bab43aa88963b1daea7f0
duleneraaa <dulener@gmail.com>
2025-02-20 00:23:13 +0530

    inside listing w
    inside listing with basic image gallery, price card, map, host details

--- RiVVe/src/App.tsx
+++ RiVVe/src/App.tsx
@@ -1,14 +1,22 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/navbar/Navbar';
 import HomePage from "./pages/HomePage";
 import About from "./pages/About";
+import Listing from "./pages/Listing";
+
 
 function App() {
   return (
     <Router>
+       <div className="min-h-screen bg-gray-100">
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing" element={<Listing/>} />
       </Routes>
+      </div>
     </Router>
   );
 }
