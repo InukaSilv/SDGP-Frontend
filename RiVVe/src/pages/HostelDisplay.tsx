aa8d75df6be7c45275c1596a38a6bebde22cdc15
Ishara Dharmapriya <isharadharmapriya145@gmail.com>
2025-03-09 15:33:42 +0530

    Gap
    map gap made

--- RiVVe/src/pages/HostelDisplay.tsx
+++ RiVVe/src/pages/HostelDisplay.tsx
@@ -109,7 +109,7 @@ const HostelDisplay: React.FC = () => {
       )}
 
       {/* map and ads */}
-      <div className="flex flex-col md:flex-row pt-40 px-4 gap-6 relative">
+      <div className="flex flex-col md:flex-row pt-40 px-4 relative">
         <div
           className={`w-full md:w-1/2 grid grid-cols-1 sm:grid-cols-2 gap-5 overflow-auto h-screen scrollbar-hidden${
             showmap ? "pointer-events-none opacity-50" : ""
@@ -123,7 +123,7 @@ const HostelDisplay: React.FC = () => {
         </div>
 
         <div
-          className={`w-full md:w-1/2 h-[65vh] fixed right-0 top-[160px] bg-white shadow-lg transition-all duration-300 ${
+          className={`w-full md:w-1/2 pl-5  fixed right-0 top-[160px] bg-white shadow-lg transition-all duration-300 ${
             showmap ? "block md:block w-full h-screen" : "hidden md:block"
           }`}
         >
