68c926dc9dc3c2403d117c68480e5e642dff1a28
Lasal0425 <lasalrathnayake@gmail.com>
2025-03-23 23:43:04 +0530

    solving error

---
+++ RiVVe/src/components/filter/FilterContent.tsx
@@ -0,0 +1,194 @@
+import { Slider } from "@mui/material";
+import { useState } from "react";
+import TypeButton from "../buttons/TypeButton";
+import Checkbox from "@mui/material/Checkbox";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { motion } from "framer-motion";
+import { X } from "lucide-react";
+
+function FilterContent({ isExpanded, setIsExpanded }) {
+  const [priceRange, setPriceRange] = useState<number[]>([30000, 60000]);
+  const [selectedHousingType, setSelectedHousingType] = useState<string[]>([]);
+  const [selectedRoomType, setSelectedRoomType] = useState<string[]>([]);
+  const [selectedFacility, setSelectedFacility] = useState<string[]>([]);
+  const options = [
+    "Date: Newest on Top",
+    "Date: Oldest on Top",
+    "Price: High to Low",
+    "Price: Low to High",
+  ];
+  const [selectedOption, setSelectedOption] = useState<string>(
+    "Date: Newest on Top"
+  );
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const changePrice = (_event: Event, newValue: number | number[]) => {
+    setPriceRange(newValue as number[]);
+  };
+
+  const toggleType = (type: string) => {
+    setSelectedHousingType((prevType) =>
+      prevType.includes(type)
+        ? prevType.filter((typ) => typ !== type)
+        : [...prevType, type]
+    );
+  };
+
+  const toggleRoomType = (type: string) => {
+    setSelectedRoomType((prevType) =>
+      prevType.includes(type)
+        ? prevType.filter((typ) => typ !== type)
+        : [...prevType, type]
+    );
+  };
+
+  const facilityAdd = (facility: string) => {
+    setSelectedFacility((prevfacility) =>
+      prevfacility.includes(facility)
+        ? prevfacility.filter((typ) => typ != facility)
+        : [...selectedFacility, facility]
+    );
+  };
+
+  return (
+    <div className="relative w-full h-full p-4 sm:p-6 bg-gray-300 text-gray-800 rounded-lg shadow-lg overflow-y-auto max-h-[90vh] mt-20 md:mt-0">
+      <button className="absolute top-4 right-4 text-gray-600 hover:text-black">
+        <X
+          size={27}
+          className="font-bold hover:cursor-pointer"
+          onClick={() => setIsExpanded(!isExpanded)}
+        />
+      </button>
+
+      <div className="w-full mb-4 mt-3">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Number of Residents
+        </label>
+        <select className="w-full p-2 bg-white rounded-md text-gray-800 border border-gray-400">
+          {[...Array(10)].map((_, i) => (
+            <option key={i} value={i + 1}>
+              {i + 1}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Price Range */}
+      <div className="w-full mb-4">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Price Range
+        </label>
+        <Slider
+          value={priceRange}
+          valueLabelDisplay="auto"
+          onChange={changePrice}
+          min={0}
+          max={100000}
+          step={5000}
+          sx={{
+            color: "#1f2937",
+            height: 6,
+          }}
+        />
+        <div className="flex justify-between text-sm sm:text-base font-medium text-gray-600">
+          <span>LKR {priceRange[0].toLocaleString()}</span>
+          <span>LKR {priceRange[1].toLocaleString()}</span>
+        </div>
+      </div>
+
+      <div className="w-full mb-4">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Housing Type
+        </label>
+        <div className="flex gap-1 flex-wrap">
+          {["Hostel", "Houses", "Apartment"].map((type) => (
+            <TypeButton
+              key={type}
+              type={type}
+              isSelected={selectedHousingType.includes(type)}
+              onClick={() => toggleType(type)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full mb-4">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Room Type
+        </label>
+        <div className="flex  flex-wrap">
+          {["Single", "Shared"].map((type) => (
+            <TypeButton
+              key={type}
+              type={type}
+              isSelected={selectedRoomType.includes(type)}
+              onClick={() => toggleRoomType(type)}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="w-full mb-4">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Facilities
+        </label>
+        <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
+          {["A/C", "CCTV", "Study Rooms", "WiFi", "Kitchen", "Food"].map(
+            (facility) => (
+              <div key={facility} className="flex items-center gap-x-2">
+                <Checkbox onChange={() => facilityAdd(facility)} />
+                <label className="text-gray-800">{facility}</label>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+
+      <div className="w-full mb-6">
+        <label className="block text-sm sm:text-base font-semibold mb-2">
+          Sort by
+        </label>
+        <div className="relative w-full">
+          <button
+            className="flex items-center justify-between w-full px-4 py-2 bg-gray-200 rounded-full text-gray-900 font-medium focus:outline-none"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            {selectedOption}
+            {isOpen ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
+          </button>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0, y: -5 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -5 }}
+              transition={{ duration: 0.2, ease: "easeOut" }}
+              className="absolute w-full bg-white rounded-md shadow-md border border-gray-400 mt-2"
+            >
+              {options.map((option) => (
+                <div
+                  key={option}
+                  className={`px-4 py-2 text-gray-900 hover:bg-gray-100 cursor-pointer`}
+                  onClick={() => {
+                    setSelectedOption(option);
+                    setIsOpen(false);
+                  }}
+                >
+                  {option}
+                </div>
+              ))}
+            </motion.div>
+          )}
+        </div>
+      </div>
+
+      <button
+        type="submit"
+        className="items-center w-full bg-blue-950 text-white p-2 rounded-2xl"
+      >
+        Apply Filters
+      </button>
+    </div>
+  );
+}
+
+export default FilterContent;
---
+++ RiVVe/src/components/teamCards/TeamCard.tsx
@@ -0,0 +1,25 @@
+type TeamMember = {
+  name: string;
+  role: string;
+  imageUrl: string;
+};
+
+const TeamCard: React.FC<TeamMember> = ({ name, role, imageUrl }) => {
+  return (
+    <div className="w-64 bg-white shadow-lg rounded-lg overflow-hidden transform hover:scale-105 transition-transform duration-300 -z-10">
+      {/* Team Member Image */}
+      <img
+        src={imageUrl}
+        alt={name}
+        className="w-full h-64 object-cover"
+      />
+      {/* Team Member Details */}
+      <div className="p-4 bg-gradient-to-r from-gray-900 to-gray-700 text-white text-center">
+        <h2 className="text-xl font-semibold">{name}</h2>
+        <p className="text-sm font-light text-gray-300">{role}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;
